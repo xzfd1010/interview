38ebf4e7abd25a2bbc2b1a12015ce9ed207f1e9f
Nick <zhongcheng509@gmail.com>
2020-03-20 17:09:30 +0800

    hot replace

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,13 +1,31 @@
 import _ from 'lodash'
+import printMe from './print'
+import './style.css'
 
 function component () {
   let element = document.createElement('div')
+  let btn = document.createElement('button')
 
   // lodash（目前通过一个 script 引入）对于执行这一行是必需的
   element.innerHTML = _.join(['Hello', 'webpack'], ' ')
   element.classList.add('hello')
 
+  btn.innerHTML = '点击这里，然后查看 console！'
+  btn.onclick = printMe
+
+  element.appendChild(btn)
   return element
 }
 
-document.body.appendChild(component())
+let element = component()
+document.body.appendChild(element)
+
+if (module.hot) {
+  module.hot.accept('./print.js', function () {
+    console.log('Accepting the updated printMe module!')
+    document.body.removeChild(element)
+    element = component() // Re-render the "component" to update the click handler
+    element = component() // 重新渲染 "component"，以便更新 click 事件处理函数
+    document.body.appendChild(element)
+  })
+}
--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,9 +1,36 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const webpack = require('webpack')
 
 module.exports = {
-  entry: './src/index.js',
+  mode: 'development',
+  entry: {
+    app: './src/index.js',
+  },
+  // devServer: {
+  //   contentBase: './dist',
+  //   hot: true,
+  // },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: '管理输出'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ],
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
   },
 }
