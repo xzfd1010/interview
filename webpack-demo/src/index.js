ebd4824ff1902bdc31ba6fe722d2dc2cabcc0dd1
Nick <zhongcheng509@gmail.com>
2020-03-20 16:12:37 +0800

    init

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,7 +1,4 @@
 import _ from 'lodash'
-import './style.css'
-import MyImage from './browserify.png'
-import Data from './data.xml';
 
 function component () {
   let element = document.createElement('div')
@@ -9,13 +6,6 @@ function component () {
   // lodash（目前通过一个 script 引入）对于执行这一行是必需的
   element.innerHTML = _.join(['Hello', 'webpack'], ' ')
   element.classList.add('hello')
-
-  var myIcon = new Image()
-  myIcon.src = MyImage
-
-  element.appendChild(myIcon)
-
-  console.log(Data);
 
   return element
 }
